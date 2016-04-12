cb06f150636b521a9edc6d0a5e1158c307762361
thomas_jakemeyn <thomas.jakemeyn@gmail.com>
2016-04-12 21:05:39 +0200

    Modified module's description in specification

--- tests/plugin.spec.js
+++ tests/plugin.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('The NightShift plugin for dependency injection', function () {
+describe('The plugin', function () {
 
     var pluginFactory = require('../src/plugin');
     var nightShift, plugin, injector, injectorFactory;
